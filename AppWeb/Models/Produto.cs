1c00ee655bad04a8e90167539ed480a6e836aaa4
Gustavo Ferreira <53050800+Gustavowp8@users.noreply.github.com>
2024-06-09 20:12:49 -0300

    Uma classe foi adicionada

---
+++ AppWeb/Models/Produto.cs
@@ -0,0 +1,39 @@
+﻿using System.Globalization;
+
+namespace AppWeb.Models
+{
+    public class Produto
+    {
+        public string Nome;
+
+        public double Preco;
+
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(int tira)
+        {
+            Quantidade -= tira;
+        }
+
+        public override string ToString()
+        {
+            return Nome
+                + ", R$ "
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Quantidade
+                + " Unidades, Total: R$ "
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
